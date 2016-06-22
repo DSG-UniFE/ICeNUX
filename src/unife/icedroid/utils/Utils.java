2f4016575d35dcdee83849c86a878d151c369b52
Alexdp74 <alessandro.morelli@unife.it>
2016-06-22 11:34:57 +0200

    Fixed missing close(); improved log messages

--- src/unife/icedroid/utils/Utils.java
+++ src/unife/icedroid/utils/Utils.java
@@ -60,6 +60,74 @@ public class Utils {
             output.flush();
             interactiveShell.waitFor();
 
+            if (DEBUG) {
+        		System.out.println("Executed command: " + commandToRun);
+        	}
+
+            //Check for errors
+            if ((line = error.readLine()) != null) {
+            	if (DEBUG) {
+	            	do {
+	            		System.err.println(line);
+	            	} while ((line = error.readLine()) != null);
+            	}
+                throw new CommandImpossibleToRun();
+            }
+            //Check to save some outputs
+            while ((line = input.readLine()) != null) {
+                results.add(line);
+            	if (DEBUG) {
+            		System.out.println(line);
+            	}
+            }
+
+        } catch (CommandImpossibleToRun citr) {
+            String msg = citr.getMessage();
+            if (DEBUG) {
+            	msg = TAG + ": " + ((msg != null) ? msg : 
+            		"rootExec() - impossible to run the command: " + commandToRun);
+            	System.err.println(msg);
+            }
+    		System.err.println("Errors detected after running command " + commandToRun);
+        	throw citr;
+        } catch (Exception ex) {
+            String msg = ex.getMessage();
+            if (DEBUG) {
+            	msg = TAG + ": " + ((msg != null) ? msg : 
+            		"rootExec() - impossible to run the command: " + commandToRun);
+            	System.err.println(msg);
+            }
+    		System.err.println("General error detected after running command " + commandToRun);
+            throw ex;
+        }
+        finally {
+            //Close all
+            input.close();
+            output.close();
+            error.close();
+            interactiveShell.destroy();
+        }
+
+        return results;
+    }
+
+    public static ArrayList<String> exec(String commandToRun) throws CommandImpossibleToRun {
+        Process process = null;
+        BufferedReader input = null;
+        BufferedReader error = null;
+        ArrayList<String> results = new ArrayList<>();
+        String line = null;
+
+        try {
+            //Run the command
+            process = Runtime.getRuntime().exec(commandToRun);
+            input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+            if (DEBUG) {
+        		System.out.println("Executed command: " + commandToRun);
+        	}
+
             //Check for errors
             if ((line = error.readLine()) != null) {
             	if (DEBUG) {
@@ -73,65 +141,20 @@ public class Utils {
             //Check to save some outputs
             while ((line = input.readLine()) != null) {
                 results.add(line);
-            }
-
-        } catch (CommandImpossibleToRun citr) {
-        	throw citr;
-        } catch (Exception ex) {
-            String msg = ex.getMessage();
-            if (DEBUG) {
-            	msg = (msg != null) ? TAG + ": " + msg : TAG + ": " + 
-            		"rootExec() - impossible to run the command: " + commandToRun;
-            	System.out.println(msg);
-            }
-            
-            throw ex;
-        }
-        finally {
-            //Close all
-            input.close();
-            output.close();
-            interactiveShell.destroy();
-        }
-
-        return results;
-    }
-
-    public static ArrayList<String> exec(String command) throws CommandImpossibleToRun {
-        Process process = null;
-        BufferedReader input = null;
-        BufferedReader error = null;
-        ArrayList<String> results = new ArrayList<>();
-
-        try {
-            //Run the command
-            process = Runtime.getRuntime().exec(command);
-            input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-            //Check for errors
-            if (error.readLine() != null) {
-                throw new CommandImpossibleToRun();
-            }
-
-            //Check to save some outputs
-            String line = null;
-            while ((line = input.readLine()) != null) {
-                results.add(line);
+            	if (DEBUG) {
+            		System.out.println(line);
+            	}
             }
 
         } catch (Exception ex) {
             String msg = ex.getMessage();
             if (DEBUG) {
-	            if (msg != null) {
-	            	msg = TAG + ": " + msg;
-	            } else {
-	            	msg = TAG + ": " + "exec() - Impossible to run the command: " + command;
-	            }
-	            System.out.println(msg);
+            	msg = TAG + ": " + ((msg != null) ? msg : 
+            		"exec() - impossible to run the command: " + commandToRun);
+	            System.err.println(msg);
             }
             
-            throw new CommandImpossibleToRun("Impossible to run the command");
+            throw new CommandImpossibleToRun("Impossible to run the command " + commandToRun);
         }
 
         return results;
