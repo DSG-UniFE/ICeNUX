cf19b6f83e9dad20d89e7970bead8eb14f67134d
Alexdp74 <alessandro.morelli@unife.it>
2016-06-23 09:56:33 +0200

    Added exception to handle unimplemented routing algorithms

---
+++ src/unife/icedroid/exceptions/RoutingAlgorithmNotFound.java
@@ -0,0 +1,11 @@
+package unife.icedroid.exceptions;
+
+public class RoutingAlgorithmNotFound extends Exception {
+	public RoutingAlgorithmNotFound() {
+		super();
+	}
+	
+	public RoutingAlgorithmNotFound(String message) {
+		super(message);
+	}
+}
