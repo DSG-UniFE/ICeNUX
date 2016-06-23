1d408b5600fa7e9e18717e4ab21e7c8a5d0a2886
Alexdp74 <alessandro.morelli@unife.it>
2016-06-23 15:35:42 +0200

    Reverted to command line script invocation

--- src/unife/icedroid/utils/NICManager.java
+++ src/unife/icedroid/utils/NICManager.java
@@ -3,9 +3,6 @@ package unife.icedroid.utils;
 import unife.icedroid.utils.OSDetector.*;
 import unife.icedroid.exceptions.*;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 /*
 import javax.script.*;
@@ -13,11 +10,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 */
-
-import javax.script.Bindings;
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 
 
 public class NICManager {
@@ -44,11 +36,10 @@ public class NICManager {
 	            			" already in ad-hoc mode and on SSID " + s.getNetworkESSID());
 	            	break;
 	            }
-	            /*
 				cmd = "/usr/bin/osascript resources/enableWiFiAdHoc " + s.getNetworkESSID() + " " + s.getNetworkChannel(); 
 				Utils.exec(cmd);
-				*/
 
+				/*
 		        BufferedReader br = new BufferedReader(new InputStreamReader(
 		        		NICManager.class.getClassLoader().getResourceAsStream("/enableWiFiAdHoc")));
 				ScriptEngineManager mgr = new ScriptEngineManager();
@@ -61,6 +52,7 @@ public class NICManager {
 				bindings.put(ScriptEngine.ARGV, s.getNetworkChannel());
 
 		        engine.eval(br, context);
+				*/
 				
                 configureNICIPAddress (s.getNetworkInterface(), s.getHostIP(),
                 		s.getNetworkMask(), s.getNetworkBroadcastAddress());
