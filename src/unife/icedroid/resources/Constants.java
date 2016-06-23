c3dad588c57bc8809f4da2c97c12dca6a5a9bd32
Alexdp74 <alessandro.morelli@unife.it>
2016-06-23 09:57:22 +0200

    Added java class to store all constants

---
+++ src/unife/icedroid/resources/Constants.java
@@ -0,0 +1,9 @@
+package unife.icedroid.resources;
+
+public class Constants {
+	
+	public static final String RESOURCES_PATH = "resources/";
+    public static final String SUBSCRIPTIONS_FILE_NAME = "resources/subscriptions.txt";
+    public static final String CONVERSATIONS_PATH = "resources/conversations/";
+
+}
